d743d0b42b6c4e699d0b5a27358e18b29474ab8b
User <hyperbolicpotato@protonmail.com>
2024-11-09 11:57:22 +0000

    display tooltip for each card in hand

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ WINDOW_HEIGHT = 900
 WINDOW_CAPTION = "Mystic Tarot Card Game Battler Thingmy"
 
 
-def draw_text(surface, text, colour, size, centre):
+def draw_text(surface, text, colour, size, centre) -> pygame.Rect:
     font = pygame.font.Font("freesansbold.ttf", size)
     text_surface = font.render(text, True, colour)
     text_rect = text_surface.get_rect(center=centre)
     surface.blit(text_surface, text_rect)
+    return text_rect
 
 MAX_CARDS = 20
 MAX_ARCANA = 3
@@ -30,12 +31,53 @@ assert MAX_CARDS - MAX_ARCANA - MAX_GENERIC == 0, f"arcana max and generic max M
 STARTING_HAND_CARD_COUNT = 5
 MAX_HEALTH = 10
 
+class Tooltip:
+    def __init__(self, upright_text, reversed_text, upright):
+        self.upright_text = upright_text
+        self.reversed_text = reversed_text
+        self.upright = upright
+        self.upright_colour = GREEN if upright else WHITE
+        self.reversed_colour = GREEN if not upright else WHITE
+
+        self.font = pygame.font.Font("freesansbold.ttf", 14)
+        self.upright_word_text_surface = self.font.render("Upright:", True, self.upright_colour)
+        self.reversed_word_text_surface = self.font.render("Reversed:", True, self.reversed_colour)
+        self.upright_text_surface = self.font.render(self.upright_text, True, self.upright_colour)
+        self.reversed_text_surface = self.font.render(self.reversed_text, True, self.reversed_colour)
+        self.upright_text_rect = self.upright_text_surface.get_rect()
+        self.reversed_text_rect = self.reversed_text_surface.get_rect()
+        self.upright_word_text_rect = self.upright_word_text_surface.get_rect()
+        self.reversed_word_text_rect = self.reversed_word_text_surface.get_rect()
+        self.box_rect = pygame.Rect(0, 0, max(self.upright_text_rect.width, self.reversed_text_rect.width), self.upright_text_rect.height + self.reversed_text_rect.height + self.upright_word_text_rect.height + self.reversed_word_text_rect.height)
+
+    def draw(self, surface, mouse_pos):
+        if self.box_rect.width > WINDOW_WIDTH - mouse_pos[0]:
+            self.box_rect.bottomright = mouse_pos
+        else:
+            self.box_rect.bottomleft = mouse_pos
+
+        self.reversed_text_rect.bottomleft = self.box_rect.bottomleft
+        self.reversed_word_text_rect.bottomleft = self.reversed_text_rect.topleft
+        self.upright_text_rect.bottomleft = self.reversed_word_text_rect.topleft
+        self.upright_word_text_rect.bottomleft = self.upright_text_rect.topleft
+
+        pygame.draw.rect(surface, BLACK, self.box_rect)
+        surface.blit(self.upright_word_text_surface, self.upright_word_text_rect)
+        surface.blit(self.upright_text_surface, self.upright_text_rect)
+        surface.blit(self.reversed_word_text_surface, self.reversed_word_text_rect)
+        surface.blit(self.reversed_text_surface, self.reversed_text_rect)
+
+
 class Competitor:
     def __init__(self):
         self.deck = []
         self.hand = []
+
         self.max_health = MAX_HEALTH
         self.health = self.max_health
+        self.health_icon_image = pygame.image.load("heart.png")
+        self.health_icon_image = pygame.transform.scale(self.health_icon_image, (39, 36))
+        self.health_icon_rect = self.health_icon_image.get_rect()
 
         self.opponent = None
 
@@ -140,7 +182,17 @@ class Competitor:
                 rect.right = self.deck_rect.left - i * self.deck_rect.width - 10 * (i + 1)
                 surface.blit(self.deck_image, rect)
 
+        for card in self.hand:
+            # render a tooltip for the card
+            if card.rect.collidepoint(mouse_pos):
+                card.tooltip.draw(surface, mouse_pos)
+
         # draw the health and lives
+        for i in range(self.health):
+            # print(self.health)
+            rect = pygame.Rect(0, self.health_icon_rect.y, self.health_icon_rect.width, self.health_icon_rect.height)
+            rect.left = self.health_icon_rect.left + i * self.health_icon_rect.width
+            surface.blit(self.health_icon_image, rect)
 
 
 class Player(Competitor):
@@ -148,6 +200,7 @@ class Player(Competitor):
         super().__init__()
         self.deck_rect.bottomleft = (20, WINDOW_HEIGHT - 20)
         self.deck_card_count_y_offset = -125
+        self.health_icon_rect.top = WINDOW_HEIGHT - 300
 
     def handle_mouse_click(self, mouse_pos):
         # saves having all the clicky logic in the main function, plus player is only thing that takes clicky input
@@ -156,12 +209,20 @@ class Player(Competitor):
             # draw a card
             self.draw_card()
 
+        for card in self.hand.copy():
+            if card.rect.collidepoint(mouse_pos) and self.num_cards_to_play_this_turn > 0:
+                # play the clicked card
+                card.play()
+                self.hand.remove(card)
+                self.num_cards_to_play_this_turn -= 1
+
 class Computer(Competitor):
     def __init__(self):
         super().__init__()
         self.deck_rect.topright = (WINDOW_WIDTH - 20, 20)
         self.deck_card_count_y_offset = 125
         self.hide_cards_in_hand = True
+        self.health_icon_rect.bottom = 300
 
 
 class Card:
@@ -173,12 +234,19 @@ class Card:
         self.played_from = played_from
         self.is_marked = False
 
+        self.upright_tooltip = ""
+        self.revered_tooltip = ""
+        self.tooltip = None
+
         self.image = pygame.image.load(image_file).convert_alpha()
         self.image = pygame.transform.scale(self.image, (100, 200))
         self.rect = self.image.get_rect()
 
         if not self.upright:
             self.image = pygame.transform.flip(self.image, False, True)
+
+    def create_tooltip(self):
+        self.tooltip = Tooltip(self.upright_tooltip, self.revered_tooltip, self.upright)
 
     def play(self):
         # override to add custom play logic
@@ -187,9 +255,11 @@ class Card:
 class GenericDamage(Card):
     def __init__(self, upright, played_on, played_from):
         super().__init__(upright, played_on, played_from, "generic-damage.png")
-
         self.damage_amount = 2
         self.heal_amount = 1
+        self.upright_tooltip = f"Deal {self.damage_amount} to opponent"
+        self.revered_tooltip = f"Heal {self.heal_amount} to yourself"
+        self.create_tooltip()
 
     def play(self):
         if self.upright:
@@ -203,9 +273,11 @@ class GenericDamage(Card):
 class GenericHeal(Card):
     def __init__(self, upright, played_on, played_from):
         super().__init__(upright, played_on, played_from, "generic-heal.png")
-
         self.damage_amount = 1
         self.heal_amount = 2
+        self.upright_tooltip = f"Heal {self.heal_amount} to yourself"
+        self.revered_tooltip = f"Damage {self.damage_amount} to opponent"
+        self.create_tooltip()
 
     def play(self):
         if self.upright:
@@ -218,20 +290,27 @@ class GenericHeal(Card):
 class TheFool(Card):
     def __init__(self, upright, played_on, played_from):
         super().__init__(upright, played_on, played_from, "TheFool.jpg")
+        self.upright_tooltip = f"Play two cards next turn"
+        self.revered_tooltip = f"Deal 1 to 4 damage to opponent, deal 4-X to yourself"
+        self.create_tooltip()
 
+        self.damage_amount = random.randint(1, 4)
+
+    def play(self):
         if self.upright:
             ##Play two cards on the Users next turn
             self.played_from.num_cards_to_play_next_turn = 2
         else:
             ##deal between 1 and 4 damage to the opponent, deals 4-X to the user
-            foolrandom: int = random.randint(1,4)
-            self.played_on.health -= foolrandom
-            self.played_from.health -= 4 - foolrandom
+            self.played_on.health -= self.damage_amount
+            self.played_from.health -= 4 - self.damage_amount
 
 class TheMagician(Card):
     def __init__(self, upright, played_on, played_from):
         super().__init__(upright, played_on, played_from, "")
+        self.create_tooltip()
 
+    def play(self):
         if self.upright:
             ##Draw 1 more card next turn
             self.played_from.num_cards_to_draw_next_turn = 2
@@ -243,7 +322,9 @@ class TheMagician(Card):
 class TheHighPriestess(Card):
     def __init__(self, upright, played_on, played_from):
         super().__init__(upright, played_on, played_from, "")
+        self.create_tooltip()
 
+    def play(self):
         if self.upright:
             pass
             ##Read and mark 1 card of the opponent
@@ -256,7 +337,11 @@ class TheHighPriestess(Card):
 class TheEmpress(Card):
     def __init__(self, upright, played_on, played_from):
         super().__init__(upright, played_on, played_from, "TheEmpress.jpg")
+        self.upright_tooltip = f"Increase max health by 2"
+        self.revered_tooltip = f"Half numerical effectiveness of next opponent card"
+        self.create_tooltip()
 
+    def play(self):
         if self.upright:
             ##Increase health by two
             self.played_from.max_health = self.played_from.max_health + 2
@@ -268,15 +353,17 @@ class TheEmpress(Card):
 class TheEmporer(Card):
     def __init__(self, upright, played_on, played_from):
         super().__init__(upright, played_on, played_from, "")
+        self.damage_amount = random.randint(1, 4)
+        self.create_tooltip()
 
+    def play(self):
         if self.upright:
             ##Block the next card the opponent plays
             pass
         else:
-            ##deal between 1 and 4 damage to the opponent, am
-            emprandom: int = random.randint(1, 4)
-            self.played_on.health -= emprandom
-            self.played_from.health -= emprandom
+            ##deal between 1 and 4 damage to the opponent, same to yourself
+            self.played_on.health -= self.damage_amount
+            self.played_from.health -= self.damage_amount
 
 ALL_ARCANA_CARDS = [TheFool, TheEmpress]
 ALL_GENERIC_CARDS = [GenericDamage, GenericHeal]
@@ -296,8 +383,11 @@ def main():
 
     player.start_turn()
 
+    fps_clock = pygame.time.Clock()
     running = True
     while running:
+        fps_clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -322,6 +412,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
